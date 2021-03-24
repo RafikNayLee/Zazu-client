01a19d854eba0a63912bc5898076ffd3e0fc3110
RafikNayLee <rfknaili@gmail.com>
2021-03-24 22:02:20 +0100

    main page filter sticky instead of fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import Tooltip from "@material-ui/core/Tooltip";
 import LinearProgress from "@material-ui/core/LinearProgress";
-
+import Divider from "@material-ui/core/Divider";
 import BottomNavigation from "@material-ui/core/BottomNavigation";
 import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
 
@@ -56,10 +56,10 @@ const useStyles = makeStyles((theme) => ({
     height: 50,
     margin: theme.spacing(1),
   },
-  spaceHolder: {
+  spaceHolder: (props) => ({
     display: "block",
-    height: 65,
-  },
+    height: props.navheight,
+  }),
   content: {
     position: "relative",
     width: `100%`,
@@ -83,8 +83,13 @@ const useStyles = makeStyles((theme) => ({
 
 const Navbar = (props) => {
   const { state, dispatch, ACTION_TYPES } = useGeneralContext();
-  const classes = useStyles();
+
   const [route, setRoute] = useState("/");
+  const [navheight, setNavheight] = useState(65);
+  const classes = useStyles({
+    navheight,
+  });
+  const toolBarRef = React.createRef();
   const { children } = props;
   useEffect(() => {
     state.user && props.history.push(route);
@@ -130,7 +135,7 @@ const Navbar = (props) => {
   return (
     <div className={classes.root}>
       <AppBar color="inherit">
-        <ToolBar className={classes.toolBar}>
+        <ToolBar className={classes.toolBar} ref={toolBarRef}>
           <div className={classes.logoArea}>
             <div className={classes.zazu}>
               <img src={zazu} alt="zazu" />
@@ -190,30 +195,32 @@ const Navbar = (props) => {
       </main>
       <div className={classes.spaceHolder}></div>
       {state.user && (
-        <BottomNavigation
-          value={route}
-          onChange={(event, newValue) => {
-            setRoute(newValue);
-          }}
-          showLabels
-          className={classes.navigation}
-        >
-          <BottomNavigationAction
-            value="/"
-            label="Home"
-            icon={icons.home({ color: "primary" })}
-          />
-          <BottomNavigationAction
-            value="/news"
-            label="News Sources"
-            icon={icons.news({ color: "primary" })}
-          />
-          <BottomNavigationAction
-            value="/categories"
-            label="Categories"
-            icon={icons.categories({ color: "primary" })}
-          />
-        </BottomNavigation>
+        <div className={classes.navigation}>
+          <Divider />
+          <BottomNavigation
+            value={route}
+            onChange={(event, newValue) => {
+              setRoute(newValue);
+            }}
+            showLabels
+          >
+            <BottomNavigationAction
+              value="/"
+              label="Home"
+              icon={icons.home({ color: "primary" })}
+            />
+            <BottomNavigationAction
+              value="/news"
+              label="News Sources"
+              icon={icons.news({ color: "primary" })}
+            />
+            <BottomNavigationAction
+              value="/categories"
+              label="Categories"
+              icon={icons.categories({ color: "primary" })}
+            />
+          </BottomNavigation>
+        </div>
       )}
     </div>
   );
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
     margin: theme.spacing(1),
   },
   paper: {
-    position: "fixed",
+    position: "sticky",
     top: 65,
     padding: theme.spacing(1),
     width: "100%",
